2c79b8732fe0467c5072666ff16413b7db2e9e17
vietdh01 <duonghoaiviet@gmail.com>
2026-04-17 23:09:16 +0700

    update version after update new module

--- spotify-elementor-sidebar-plugin/spotify-elementor-sidebar-menu.php
+++ spotify-elementor-sidebar-plugin/spotify-elementor-sidebar-menu.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Ultimate Index Addon-ons
  * Description: Adds a Spotify-style sidebar menu widget for Elementor.
- * Version: 1.2.3
+ * Version: 1.2.4
  * Author: Copilot
  * Requires at least: 6.0
  * Requires PHP: 7.4
